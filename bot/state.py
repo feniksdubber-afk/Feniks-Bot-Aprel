6dc316fc48f47cd34c8bf371114aba9a4eba19ce
feniksdubber-afk <feniksdubber@gmail.com>
2026-04-25 11:19:32 +0500

    Initialize user states and sessions in state.py
    Added user_states and user_sessions dictionaries to manage user states and session data.

--- bot/state.py
+++ bot/state.py
@@ -1,1 +1,7 @@
+# Bot/state.py
+
+# Foydalanuvchi qaysi bosqichda ekanligini saqlash uchun
 user_states = {}
+
+# Tizimga kirgan foydalanuvchi ma'lumotlarini saqlash uchun (Xatoni tuzatuvchi qator)
+user_sessions = {}
