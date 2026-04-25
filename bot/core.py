fdb5d341758af377bef773c57a05e93c067f0024
feniksdubber-afk <feniksdubber@gmail.com>
2026-04-25 11:43:11 +0500

    Update core.py

--- bot/core.py
+++ bot/core.py
@@ -2,14 +2,22 @@ import telebot
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # .env faylini o'qiydi
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
+bot = telebot.TeleBot(TOKEN)
 
+# Modullarni import qilish (Circular import bo'lmasligi uchun pastda)
 from bot import auth
 auth.register(bot)
 
 def start_bot():
-    print("🚀 Bot ishga tushdi")
-    bot.infinity_polling(skip_pending=True)
+    try:
+        print("✅ Bot pollingni boshlamoqda...")
+        # skip_pending=True - bot o'chib qolgan vaqtda yozilgan eski xabarlarga javob bermasligi uchun
+        bot.infinity_polling(skip_pending=True)
+    except Exception as e:
+        print(f"❌ Bot pollingda xatolik: {e}")
+
+if __name__ == "__main__":
+    start_bot()
